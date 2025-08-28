5f067027db5b62e8f86453e78b70f2fb10e462e4
MURAKAMI Masahiko <m-murakami@esm.co.jp>
2025-08-28 09:02:53 +0900

    refactor: update file fetching logic and enhance file display

--- src/components/FileList.tsx
+++ src/components/FileList.tsx
@@ -1,22 +1,37 @@
-import React, { useState, useEffect } from 'react';
-import { List, ListItem, ListItemIcon, ListItemText, Paper, Typography } from '@mui/material';
-import InsertDriveFileIcon from '@mui/icons-material/InsertDriveFile';
-import { client } from '../amplify';
-import type { Schema } from '../../amplify/data/resource';
+import React, { useState, useEffect } from "react";
+import {
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Paper,
+  Typography,
+} from "@mui/material";
+import InsertDriveFileIcon from "@mui/icons-material/InsertDriveFile";
+import { client } from "../amplify";
+import type { Schema } from "../../amplify/data/resource";
+
+type FileWithFolder = Pick<
+  Schema["File"]["type"],
+  "id" | "name" | "content"
+> & { folder: Pick<Schema["Folder"]["type"], "name"> };
 
 const FileList: React.FC = () => {
-  const [files, setFiles] = useState<Array<Schema["File"]["type"]>>([]);
+  const [files, setFiles] = useState<Array<FileWithFolder>>([]);
 
   useEffect(() => {
-    const sub = client.models.File.observeQuery().subscribe({
-      next: (data) => setFiles([...data.items]),
-    });
+    const fetchFiles = async () => {
+      const { data: items } = await client.models.File.list({
+        selectionSet: ["id", "name", "content", "folder.name"],
+      });
+      setFiles(items);
+    };
 
-    return () => sub.unsubscribe();
+    fetchFiles();
   }, []);
 
   return (
-    <Paper elevation={3} style={{ margin: '20px', padding: '20px' }}>
+    <Paper elevation={3} style={{ margin: "20px", padding: "20px" }}>
       <Typography variant="h5" gutterBottom>
         File List
       </Typography>
@@ -26,7 +41,14 @@ const FileList: React.FC = () => {
             <ListItemIcon>
               <InsertDriveFileIcon />
             </ListItemIcon>
-            <ListItemText primary={file.name} secondary={file.content} />
+            <ListItemText
+              primary={file.name}
+              secondary={
+                file.folder.name
+                  ? `Folder: ${file.folder.name} | ${file.content}`
+                  : file.content
+              }
+            />
           </ListItem>
         ))}
       </List>
