eefdc25f56a5647bf1db860c7a107c1b98b2b1d7
MURAKAMI Masahiko <m-murakami@esm.co.jp>
2025-09-01 21:29:32 +0900

    feat: replace sign out button with AppBar and Toolbar for improved UI

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,23 @@ import { Authenticator } from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css";
 import "./App.css";
 import FileList from "./components/FileList";
+import { AppBar, Toolbar, Typography, Button } from "@mui/material";
 
 function App() {
   return (
     <Authenticator>
       {({ signOut }) => (
         <div className="App">
-          <header>
-            <button onClick={signOut}>Sign out</button>
-          </header>
+          <AppBar position="static">
+            <Toolbar>
+              <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                File Storage
+              </Typography>
+              <Button color="inherit" onClick={signOut}>
+                Sign out
+              </Button>
+            </Toolbar>
+          </AppBar>
           <FileList />
         </div>
       )}
